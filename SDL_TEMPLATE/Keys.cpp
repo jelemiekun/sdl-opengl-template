e396299903b2eb612703f94540c00d85b65efa67
jelemiekun - pc <jeremieeden1946@gmail.com>
2025-04-02 18:44:29 +0800

    fix: Keys handleInput function definition

--- SDL_TEMPLATE/Keys.cpp
+++ SDL_TEMPLATE/Keys.cpp
@@ -1,6 +1,12 @@
 #include "Keys.h"
+#include <spdlog/spdlog.h>
 
 Keys* Keys::getInstance() {
     static Keys instance;  // Guaranteed to be created only once
     return &instance;  // Return raw pointer to the instance
+}
+
+void Keys::handleInput(SDL_Event& gEvent) {
+    if (gEvent.type == SDL_KEYDOWN)
+        spdlog::info("Key Pressed: {}", gEvent.key.keysym.sym);
 }
